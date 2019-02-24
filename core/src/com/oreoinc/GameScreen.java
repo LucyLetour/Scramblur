b7a02dd1f2b6d853278996bdc37a4eb50913f22c
Luke Letourneau <LukeLetourneau2000@gmail.com>
2019-02-24 03:43:39 -0500

    Work :flex:

--- core/src/com/oreoinc/GameScreen.java
+++ core/src/com/oreoinc/GameScreen.java
@@ -1,32 +1,94 @@
 package com.oreoinc;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class GameScreen implements Screen {
     private Image currentImage;
-    private long timeElapsedMs;
-    private long scrambleTimeElapsed;
+    private double timeElapsedS;
+    private double scrambleTimeElapsed;
     private boolean timeHasElapsed;
 
-    private final long ALLOTED_TIME_MS = 10000; //10 seconds
-    private final long SCRAMBLE_TIME_MS = 500; //0.5 seconds
-    private final long WAIT_TIME_MS = 2000; // 2 seconds
+    private final double ALLOTED_TIME_S = 10; //10 seconds
+    private final double SCRAMBLE_TIME_S = 0.5; //0.5 seconds
+    private final double WAIT_TIME_S = 2; // 2 seconds
 
     private final double MAX_RADIUS = 100;
     private final double MIN_RADIUS = 0;
 
+    private int points;
+
+    private ArrayList<Image> images;
+
+    final Label timeLeft;
+    final Label pointsGained;
+
+    private Stage stage;
+    private SpriteBatch batch;
+    private Texture image;
+
     public enum Image {
-        CAMERA, DOG, ELEPHANT, FISH, TAJ_MAHAL, TAXI;
+        CAMERA("camera"), DOG("dog"), ELEPHANT("elephant"), FISH("fish"), TAJ_MAHAL("taj mahal"), TAXI("taxi");
+
+        String guessKey;
+
+        Image(String guessKey) {
+            this.guessKey = guessKey;
+        }
+
+        public String getGuessKey() {
+            return guessKey;
+        }
     }
 
     public GameScreen() {
-        ArrayList<Image> images = new ArrayList<Image>();
+        images = new ArrayList<Image>();
+        Collections.addAll(images, Image.values());
         Collections.shuffle(images);
         currentImage = images.remove(0);
         timeHasElapsed = false;
+        batch = new SpriteBatch();
+        points = 0;
+        stage = new Stage();
+        Skin skin = new Skin(Gdx.files.internal("skins/skin/uiskin.json"));
+        final TextField textField = new TextField("", skin);
+        textField.setPosition(5, 5);
+        textField.setSize(100, 20);
+        textField.addListener(new InputListener() {
+            @Override
+            public boolean keyUp(InputEvent event, int keycode) {
+                if(keycode == Input.Keys.ENTER && checkAnswer(textField.getText()) && !timeHasElapsed) {
+                    points++;
+                    timeHasElapsed = true;
+                    timeElapsedS = 0;
+                    textField.setText("");
+                }
+                return true;
+            }
+        });
+
+        timeLeft = new Label("", skin);
+        timeLeft.setPosition(800, 0);
+        timeLeft.setSize(50, 20);
+
+        pointsGained = new Label("", skin);
+        pointsGained.setPosition(450, 0);
+        pointsGained.setSize(100, 20);
+
+        stage.addActor(timeLeft);
+        stage.addActor(pointsGained);
+        stage.addActor(textField);
+        Gdx.input.setInputProcessor(this.stage);
     }
 
     @Override
@@ -36,32 +98,45 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        image = new Texture(Gdx.files.internal("pictures/" + currentImage.name() + ".jpg"));//scramble calculateRadius();
+
+
+
         if(!timeHasElapsed) {
-            if(timeElapsedMs > ALLOTED_TIME_MS) {
-                timeElapsedMs = 0;
+            if(timeElapsedS > ALLOTED_TIME_S) {
+                timeElapsedS = 0;
                 timeHasElapsed = true;
             }
             else {
-                if(scrambleTimeElapsed > SCRAMBLE_TIME_MS) {
-                    //scramble calculateRadius();
+                if(scrambleTimeElapsed > SCRAMBLE_TIME_S) {
+
                 }
             }
         }
         else {
-            if (timeElapsedMs > WAIT_TIME_MS) {
-                timeElapsedMs = 0;
+            if (timeElapsedS > WAIT_TIME_S) {
+                timeElapsedS = 0;
                 timeHasElapsed = false;
+                currentImage = images.remove(0);
             }
         }
 
-        timeElapsedMs += delta;
+        timeElapsedS += delta;
         scrambleTimeElapsed += delta;
 
-        
+        batch.begin();
+        batch.draw(image, 0, 32, 992, 558);
+        batch.end();
+
+        pointsGained.setText("Points: " + points);
+        timeLeft.setText("Time remaining: " + Math.round((timeHasElapsed ? WAIT_TIME_S : ALLOTED_TIME_S) - timeElapsedS));
+
+        stage.draw();
+        stage.act();
     }
 
     public double calculateRadius() {
-        double alpha = (double)timeElapsedMs / ALLOTED_TIME_MS;
+        double alpha = timeElapsedS / ALLOTED_TIME_S;
         return (1 - alpha) * MAX_RADIUS + alpha * MIN_RADIUS;
     }
 
@@ -88,5 +163,12 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
 
+    }
+    private boolean checkAnswer(String guess) {
+        if(guess.toLowerCase().trim().equals(currentImage.getGuessKey())) {
+            timeHasElapsed = true;
+            return true;
+        }
+        return false;
     }
 }
--- desktop/src/com/oreoinc/desktop/DesktopLauncher.java
+++ desktop/src/com/oreoinc/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.oreoinc.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 992;
+		config.height = 600;
 		new LwjglApplication(new Main(), config);
 	}
 }
