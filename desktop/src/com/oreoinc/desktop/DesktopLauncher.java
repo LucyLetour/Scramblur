d8bc65e5b055c3fcbb13595d7ffeb04264013fe3
Luke Letourneau <LukeLetourneau2000@gmail.com>
2019-02-23 23:27:19 -0500

    Initial Commit

---
+++ desktop/src/com/oreoinc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.oreoinc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.oreoinc.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
