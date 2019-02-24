621b901f01dde00611a0a52a28801b735edf8068
Luke Letourneau <LukeLetourneau2000@gmail.com>
2019-02-24 11:26:05 -0500

    updated end screen

--- core/src/com/oreoinc/EndScreen.java
+++ core/src/com/oreoinc/EndScreen.java
@@ -15,14 +15,17 @@ public class EndScreen implements Screen {
     private int finalScore;
 
     public EndScreen(int finalScore) {
-        batch = new SpriteBatch();
         this.finalScore = finalScore;
+
+        batch = new SpriteBatch();
+
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("Hacked.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = 30;
         font = generator.generateFont(parameter); // font size 30 pixels
         parameter.size = 240;
         font2 = generator.generateFont(parameter);
+
         generator.dispose();
     }
 
@@ -34,10 +37,14 @@ public class EndScreen implements Screen {
     @Override
     public void render(float delta) {
         batch.begin();
+
         font.setColor(Color.WHITE);
         font.draw(batch, "Congratulations! Your final score was:", 250, 450);
+
+        //Render final score
         font2.setColor(Color.LIME);
         font2.draw(batch, finalScore + "", 450, 400);
+
         batch.end();
     }
 
